682fb16ed181f4e82681a8922f35f5577fd66e20
Ken Shamrock Dizon <dizonshamrock@gmail.com>
2024-11-09 15:51:35 +0800

    Implement method to check if user already exists

--- src/main/java/com/vettercare/vettercare/repository/OwnerRepository.java
+++ src/main/java/com/vettercare/vettercare/repository/OwnerRepository.java
@@ -4,6 +4,9 @@ import com.vettercare.vettercare.model.owner.Owner;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface OwnerRepository extends JpaRepository<Owner, Long> {
+    Optional<Owner> findByOwnerFirstNameAndOwnerLastNameAndAddress(String ownerFirstName, String ownerLastName, String address);
 }
