001c01554e51e2b352391f1b9bdc4df581c6cf47
Ken Shamrock Dizon <dizonshamrock@gmail.com>
2024-11-09 19:33:23 +0800

    Implement filtering according to species, breed or both

--- src/main/java/com/vettercare/vettercare/controller/PatientController.java
+++ src/main/java/com/vettercare/vettercare/controller/PatientController.java
@@ -1,9 +1,16 @@
 package com.vettercare.vettercare.controller;
 
+import com.vettercare.vettercare.model.patient.Patient;
 import com.vettercare.vettercare.service.PatientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/patients")
@@ -16,4 +23,18 @@ public class PatientController {
         this.patientService = patientService;
     }
 
+    @GetMapping
+    public ResponseEntity<List<Patient>> getPatients(
+            @RequestParam(required = false) String species,
+            @RequestParam(required = false) String breed
+    ) {
+        List<Patient> patients = patientService.getPatients(species, breed);
+
+        if (patients.isEmpty()) {
+            System.out.println("No patients");
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+
+        return new ResponseEntity<>(patients, HttpStatus.OK);
+    }
 }
--- src/main/java/com/vettercare/vettercare/repository/PatientRepository.java
+++ src/main/java/com/vettercare/vettercare/repository/PatientRepository.java
@@ -4,6 +4,14 @@ import com.vettercare.vettercare.model.patient.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PatientRepository extends JpaRepository<Patient, Long> {
+
+    List<Patient> filterBySpecies(String species) ;
+
+    List<Patient> filterByBreed(String breed);
+
+    List<Patient> filterBySpeciesAndBreed(String species, String breed);
 }
